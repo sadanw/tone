6800debe1ba085cc40521c36caba588f3f2e041b
sadanw <sada.r.nichols-worley.22@dartmouth.edu>
2020-07-14 23:41:23 -0400

    Adding react to starter pack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
+
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
 
 let time = 0;
 setInterval(() => {
